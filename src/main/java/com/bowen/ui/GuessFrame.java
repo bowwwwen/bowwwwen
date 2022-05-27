81f864a739ea729a3a628a4e4e1e5ce3e910fe67
bowen <08bowen@gmail.com>
2022-05-27 13:05:40 +0800

    0527 class (guess number)

--- src/main/java/com/bowen/ui/GuessFrame.java
+++ src/main/java/com/bowen/ui/GuessFrame.java
@@ -4,33 +4,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
-
+    Random random = new Random();
+    int secret = random.nextInt(10)+1;
+    JButton button = new JButton("ENTER");
+    JLabel label = new JLabel("Guess number 1~10");
+    JTextField number = new JTextField(10);
 
     public GuessFrame(){
         super();
+        System.out.println(secret);
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setVisible(true);
-        JButton button = new JButton("another");
-        JLabel label = new JLabel("helllo3");
-        button.addActionListener(0527new ActionListener() {
+        button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("hello2");
-                label.setText("ni hao");
+//                System.out.println("hello2");
+//                label.setText("ni hao");
+
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+                if (num<secret){
+                    label.setText("bigger");
+                }else if (num>secret){
+                    label.setText("smaller");
+                }else {
+                    label.setText("bingo" + "\t" + "secret number is " + "\t" + secret);
+                }
             }
         });
 
-
         setLayout(new FlowLayout());
+        add(number);
         add(label);
         add(button);
         setVisible(true);
-
-
     }
 
     public static void main(String[] args) {
